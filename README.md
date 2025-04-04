9b36810482ae3b141253cc6d3cb0cb9302be9d66
Gulshanstar <gulshanmundri64@gmail.com>
2025-04-04 21:35:59 +0530

    firstProgramOfNode

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CompleteBackendForFrontEnd
+
+In this course we will learn Complete backend i.e - 
+Nodejs, Expressjs(Backend Framework) & withMongodb
+
---
+++ firstProgramOfNode.js
@@ -0,0 +1,10 @@
+// Node is written in c++ language and used google v8 java script engine.
+
+function hey(name){
+    console.log("Your name is:"+ name)
+}
+
+hey('gulshan')
+
+// ek baat ka dhiyan rakhe ki node me window and document object nhi hotta hai 
+// jaise ki Browser ke saat hotta hai
